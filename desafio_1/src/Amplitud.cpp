343494de91f249ca08ce0f5ed177f59ab051c60b
carlos mario rendon martinez <carlos.rendonm1@udea.edu.co>
2025-04-22 21:10:29 -0500

    Update Amplitud.cpp

--- desafio_1/src/Amplitud.cpp
+++ desafio_1/src/Amplitud.cpp
@@ -1,7 +1,7 @@
-// Leemos la primera línea: la semilla
+
 archivo >> seed;
 
-// Vamos a contar cuántos píxeles (tripletas RGB) tiene el archivo
+
 int r, g, b;
 n_pixels = 0;
 while (archivo >> r >> g >> b) {
@@ -9,20 +9,20 @@ while (archivo >> r >> g >> b) {
 }
 archivo.close();
 
-// Reabrimos el archivo para leer los valores ahora que ya sabemos cuántos hay
+
 archivo.open(nombreArchivo);
 if (!archivo.is_open()) {
     cout << "Error al reabrir el archivo de máscara." << endl;
     return nullptr;
 }
 
-// Leemos otra vez la semilla (porque tocaba abrirlo desde cero)
+
 archivo >> seed;
 
-// Creamos el arreglo para guardar los datos RGB
+
 unsigned int* data = new unsigned int[n_pixels * 3];
 
-// Leemos todos los valores RGB
+
 for (int i = 0; i < n_pixels * 3; i = i + 3) {
     archivo >> r >> g >> b;
     data[i] = r;
