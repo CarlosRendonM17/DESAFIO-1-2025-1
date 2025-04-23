b79a83d778f33d6b46e293e700a943e85ff577f5
carlos mario rendon martinez <carlos.rendonm1@udea.edu.co>
2025-04-22 22:06:29 -0500

    Update main_caso2.cpp

--- desafio_1/src/main_caso2.cpp
+++ desafio_1/src/main_caso2.cpp
@@ -1,43 +1,31 @@
-#include <QString>
-#include <iostream>
-#include "funciones.h"
 
-using namespace std;
+QString imagenXOR = "data/Caso2/I_M.bmp";
 
-int main() {
-    
-    QString imagenEntrada = "DesafíoI/Caso 2/I_D.bmp";
 
-   
-    QString imagenXOR = "DesafíoI/Caso 2/I_M.bmp";
+QString ordenes[] = {"xor-rotar", "rotar-xor", "desplazar-xor"};
 
- 
-    QString ordenes[] = {"xor-rotar", "rotar-xor", "desplazar-xor"};
 
-    bool encontrado = false; 
+bool encontrado = false;
+
+
+for (int i = 0; i <= 6 && encontrado == false; i = i + 1) {
+    QString archivoMascara = QString("data/Caso2/M%1.txt").arg(i);
 
   
-    for (int i = 0; i <= 6 && encontrado == false; i = i + 1) {
-       
-        QString archivoMascara = QString("DesafíoI/Caso 2/M%1.txt").arg(i);
+    for (int j = 0; j < 3 && encontrado == false; j = j + 1) {
+        QString orden = ordenes[j];
 
-     
-        for (int j = 0; j < 3 && encontrado == false; j = j + 1) {
-            QString orden = ordenes[j];
-
-           
-            if (probarOrdenTransformaciones(imagenEntrada, imagenXOR, archivoMascara.toStdString().c_str(), orden)) {
-                cout << "Transformación correcta con orden: " << orden.toStdString()
-                     << " y máscara: " << archivoMascara.toStdString() << endl;
-                encontrado = true;
-            }
+        if (probarOrdenTransformaciones(imagenEntrada, imagenXOR, archivoMascara.toStdString().c_str(), orden)) {
+            cout << "Transformación correcta con orden: " << orden.toStdString()
+                 << " y máscara: " << archivoMascara.toStdString() << endl;
+            encontrado = true;
         }
     }
-
-
-    if (encontrado == false) {
-        cout << "Ninguna combinación fue válida para Caso 2." << endl;
-    }
-
-    return 0;
 }
+
+
+if (encontrado == false) {
+    cout << "Ninguna combinación fue válida para Caso 2." << endl;
+}
+
+return 0;
