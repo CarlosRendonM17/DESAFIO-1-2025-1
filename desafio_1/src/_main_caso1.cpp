df49bf2491799a63f9333c8d8c5c70a06bd3f4b6
carlos mario rendon martinez <carlos.rendonm1@udea.edu.co>
2025-04-22 22:08:17 -0500

    Update _main_caso1.cpp

--- desafio_1/src/_main_caso1.cpp
+++ desafio_1/src/_main_caso1.cpp
@@ -1,37 +1,28 @@
-#include <QString>
-#include <iostream>
-#include "funciones.h"
 
-using namespace std;
+QString IM = "data/Caso1/I_M.bmp";
 
-int main() {
-   
-    QString ID = "data/ID.bmp";
 
-   
-    QString IM = "data/IM.bmp";
+const char* M1 = "data/Caso1/M1.txt";
 
-   
-    const char* M1 = "data/M1.txt";
 
- 
-    QString ordenes[] = {"xor-rotar", "rotar-xor", "desplazar-xor"};
+QString ordenes[] = {"xor-rotar", "rotar-xor", "desplazar-xor"};
 
-    bool encontrado = false; 
-    
-   
-    for (const QString& orden : ordenes) {
-        if (probarOrdenTransformaciones(ID, IM, M1, orden)) {
-            cout << "Transformación correcta con orden: " << orden.toStdString() << endl;
-            encontrado = true;
-            break;
-        }
+
+bool encontrado = false;
+
+
+for (int i = 0; i < 3 && encontrado == false; i = i + 1) {
+    QString orden = ordenes[i];
+
+    if (probarOrdenTransformaciones(ID, IM, M1, orden)) {
+        cout << "Transformación correcta con orden: " << orden.toStdString() << endl;
+        encontrado = true;
     }
-
-   
-    if (encontrado == false) {
-        cout << "Ninguna combinación fue válida." << endl;
-    }
-
-    return 0;
 }
+
+
+if (encontrado == false) {
+    cout << "Ninguna combinación fue válida." << endl;
+}
+
+return 0;
